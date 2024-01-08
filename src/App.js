c0b0d3aeb4306d39ee2fd988fd6e17fb31611d4c
Kandeeparaja Vithooran <vithoo24@gmail.com>
2024-01-08 11:58:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Button from './button';
+import Header from './Header';
+import Body from './Body';
+import Footer from './footer'
+function App() {
+  return (
+    <div>
+ <Header/>
+ <Body/>
+ <Footer/>
+ </div>
+  );
+}
+
+export default App;
---
+++ src/button.js
@@ -0,0 +1,23 @@
+
+// import './App.css';
+
+// function Button() {
+//   return (
+//     <div className="Button">
+      
+//       <button>submit</button>
+//       <Text></Text>
+//     </div>
+//   );
+// }
+
+// function Text() {
+//     return (
+//       <div className="Text">
+        
+//         <p>My name is Shanuka</p>
+//       </div>
+//     );
+//   }
+
+// export default Button;
